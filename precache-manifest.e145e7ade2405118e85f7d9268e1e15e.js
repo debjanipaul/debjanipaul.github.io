796eb43ef6e6f2479404f8698224bb2e77fc8627
Debjani Paul <debjanideb@hotmail.com>
2020-04-10 22:14:42 -0700

    Updates

---
+++ precache-manifest.e145e7ade2405118e85f7d9268e1e15e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cf42bfe3ce0e7dd9af1166f2ed9c4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f249bf7bc1aab8f0972",
+    "url": "/static/css/main.08c0f1fc.chunk.css"
+  },
+  {
+    "revision": "31b47e61d8e22191ff29",
+    "url": "/static/js/2.2457e914.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2457e914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f249bf7bc1aab8f0972",
+    "url": "/static/js/main.cd34797f.chunk.js"
+  },
+  {
+    "revision": "efc26e94151472dc952d",
+    "url": "/static/js/runtime-main.d73748ce.js"
+  },
+  {
+    "revision": "cad93160148744e9b3dd56f26cf8e2a8",
+    "url": "/static/media/ArcadeCloneGame.cad93160.png"
+  },
+  {
+    "revision": "09aeab847260a350c73e2e547dce6cd9",
+    "url": "/static/media/Bootstrap.09aeab84.png"
+  },
+  {
+    "revision": "9f5fc493a4feaf3b32cfab0bfc6bb28a",
+    "url": "/static/media/BootstrapLandingPage.9f5fc493.png"
+  },
+  {
+    "revision": "fbe01551b3091103d2655e6be9d8cd9f",
+    "url": "/static/media/CSS3.fbe01551.png"
+  },
+  {
+    "revision": "42e129cfbde25458e54629b16463ba52",
+    "url": "/static/media/RestaurantReviews2.42e129cf.png"
+  },
+  {
+    "revision": "a04aeea7b867f59413ac0e548e30a4ca",
+    "url": "/static/media/chefMaker.a04aeea7.png"
+  },
+  {
+    "revision": "2be40143164894050f41b72f551c500c",
+    "url": "/static/media/colorGame.2be40143.png"
+  },
+  {
+    "revision": "d638bd34b425dec39792d4a81189eddf",
+    "url": "/static/media/express.d638bd34.png"
+  },
+  {
+    "revision": "c8a83d0788097e656bc0129ba6021912",
+    "url": "/static/media/github1.c8a83d07.png"
+  },
+  {
+    "revision": "e4b078058b9a596a0bdaa41bd70540db",
+    "url": "/static/media/jQuery.e4b07805.png"
+  },
+  {
+    "revision": "7fa98f28ae61a59d308bfae2b94096a4",
+    "url": "/static/media/jquery.7fa98f28.jpg"
+  },
+  {
+    "revision": "b4cc9b6dc76bcc06f3d83656720ebde7",
+    "url": "/static/media/js.b4cc9b6d.png"
+  },
+  {
+    "revision": "30c27024412cc74f4d9e662d7de31e80",
+    "url": "/static/media/memeGenerator.30c27024.png"
+  },
+  {
+    "revision": "ae631d24b7ea1f1fbe93b4bc8a560947",
+    "url": "/static/media/memoryGame.ae631d24.png"
+  },
+  {
+    "revision": "b10b140c6a5e314a0eccf161c5d4f112",
+    "url": "/static/media/mongo.b10b140c.png"
+  },
+  {
+    "revision": "d42672d4d185739d26257ed5c653c740",
+    "url": "/static/media/node.d42672d4.png"
+  },
+  {
+    "revision": "b2d32d3fc54cd8859e4b2518e49145fe",
+    "url": "/static/media/portfolioPic.b2d32d3f.jpg"
+  },
+  {
+    "revision": "b61c4fbeecd73fc412296f8f182c94bd",
+    "url": "/static/media/profilePic2.b61c4fbe.svg"
+  },
+  {
+    "revision": "692adb35708d9279ca7eb56c71bfc102",
+    "url": "/static/media/react2.692adb35.png"
+  }
+]);
